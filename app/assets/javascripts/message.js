910ccac3245bb3e6d24576204d6785392c2ce11b
hase07180 <yuukisoccer07180@gmail.com>
2020-07-06 20:12:39 +0900

    Edit Ajax

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,66 @@
+$(function(){
+  function buildHTML(message){
+    if ( message.image ) {
+      let html =
+        `<div class="message-area__box">
+          <div class="message-area__box__user-info">
+            <div class="message-area__box__user-info__name">
+              ${message.user_name}
+            </div>
+            <div class="message-area__box__user-info__date">
+              ${message.created_at}
+            </div>
+          </div>
+          <div class="message-area__box__user-message">
+            <p class="message-area__box__user-message__content">
+              ${message.message}
+            </p>
+            <img class="Message__image" src="${message.image}">
+          </div>
+        </div>`
+      return html;
+    } else {
+      let html =
+      `<div class="message-area__box">
+        <div class="message-area__box__user-info">
+          <div class="message-area__box__user-info__name">
+            ${message.user_name}
+          </div>
+          <div class="message-area__box__user-info__date">
+            ${message.created_at}
+          </div>
+        </div>
+        <div class="message-area__box__user-message">
+          <p class="Message__image">
+            ${message.message}
+          </p>
+        </div>
+      </div>`
+      return html;
+    };
+  }
+
+  $('.footer-form').on('submit', function(e) {
+    e.preventDefault()
+    let formData = new FormData(this);
+    let url = $(this).attr('action');
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      let html = buildHTML(data);
+      $('.message-area').append(html);
+      $('form')[0].reset();
+      $('.message-area').animate({ scrollTop: $('.message-area')[0].scrollHeight});
+      $('.submit-buttun').prop('disabled', false);
+    })
+    .fail(function(){
+      alert('メッセージ送信に失敗しました');
+    })
+  })
+});
